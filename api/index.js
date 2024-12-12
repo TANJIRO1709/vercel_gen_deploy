ac86e711cdee5c42a234482d0341514fad89e1e3
Sujal Kumar Agarwal <sujalkumaragarwal911@Developer-Kit.local>
2024-12-12 09:16:51 +0530

    vercel deploy

--- api/index.js
+++ api/index.js
@@ -2,22 +2,32 @@ import express from "express";
 import { config } from "dotenv";
 import jsonParser from "../middlewares/jsonParser.js";
 import corsConfig from "../middlewares/cors.js";
-import serviceHubAi from "../routes/serviceHubAi.js"
+import serviceHubAi from "../routes/serviceHubAi.js";
 
 const app = express();
-app.use(express.json());
+
+// Load environment variables
 config();
+
+// Middleware
+app.use(express.json());
+app.use(corsConfig);
+app.use(jsonParser);
+
+// Define routes
 const port = process.env.PORT || 5000;
-app.use(corsConfig)
-app.use(jsonParser)
+
 app.get("/", (req, res) => {
     res.send({
         message: "Project NEXTGEN Server",
         health: "ok",
     });
 });
+
+// Route for ServiceHubAI
 app.use("/api/ServiceHubAI", serviceHubAi);
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
